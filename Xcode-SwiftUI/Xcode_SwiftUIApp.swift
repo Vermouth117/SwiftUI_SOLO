65b820058c67b87ff5191ae51407b5bd512a8183
Vermouth117 <e153jsb7eg@gmail.com>
2023-06-12 17:43:56 +0900

    Initial Commit

---
+++ Xcode-SwiftUI/Xcode_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_SwiftUIApp.swift
+//  Xcode-SwiftUI
+//
+//  Created by Vermouth on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
