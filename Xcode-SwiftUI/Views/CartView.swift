3470621f3cad16c10ab198ecb4e00445c1772c66
Vermouth117 <e153jsb7eg@gmail.com>
2023-06-16 00:22:35 +0900

    user control

--- Xcode-SwiftUI/Views/CartView.swift
+++ Xcode-SwiftUI/Views/CartView.swift
@@ -11,6 +11,8 @@ import FirebaseFirestore
 struct CartView: View {
     @State private var documents: [DocumentSnapshot] = []
     
+    @AppStorage("uid") var userID: String = ""
+    
     var body: some View {
         VStack {
             Text("Cart")
@@ -22,73 +24,80 @@ struct CartView: View {
             Spacer()
             
             ScrollView (.vertical, showsIndicators: false) {
-                ForEach(Array(documents.enumerated()), id: \.element.documentID) { index, flower in
-                    if let title = flower.data()?["title"] as? String,
-                       let price = flower.data()?["price"] as? Int,
-                       let image = flower.data()?["image"] as? String,
-                       let star = flower.data()?["star"] as? Double {
-                        HStack {
-                            VStack {
-                                Image(image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 100, height: 130)
-                                    .cornerRadius(10)
-                            }
-                            .frame(width: 100, height: 100)
-                            
-                            VStack (alignment: .leading) {
-                                Text(title)
-                                    .font(.title)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color(red: 0.8, green: 0.6, blue: 0.7))
-//                                    .padding(.bottom, 20)
-                                
-                                HStack {
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow.opacity(0.9))
-                                    Text(String(format: "%.1f", star))
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(Color(red: 0.8, green: 0.6, blue: 0.7, opacity: 0.7))
-//                                        .padding(.top, 1)
+                if (documents.count == 0) {
+                    Text("カートは空です。")
+                        .font(.title2)
+                        .foregroundColor(.gray)
+                } else {
+                    ForEach(Array(documents.enumerated()), id: \.element.documentID) { index, flower in
+                        if let title = flower.data()?["title"] as? String,
+                           let price = flower.data()?["price"] as? Int,
+                           let image = flower.data()?["image"] as? String,
+                           let star = flower.data()?["star"] as? Double {
+                            HStack {
+                                VStack {
+                                    Image(image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 100, height: 130)
+                                        .cornerRadius(10)
                                 }
-//                                .padding(.bottom, 40)
+                                .frame(width: 100, height: 100)
                                 
-                                HStack {
-                                    Text("$\(price)")
+                                VStack (alignment: .leading) {
+                                    Text(title)
                                         .font(.title)
-                                        .fontWeight(.bold)
-                                        .foregroundColor(Color(red: 0.8, green: 0.5, blue: 0.1))
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(Color(red: 0.8, green: 0.6, blue: 0.7))
+    //                                    .padding(.bottom, 20)
                                     
-                                    Spacer()
+                                    HStack {
+                                        Image(systemName: "star.fill")
+                                            .foregroundColor(.yellow.opacity(0.9))
+                                        Text(String(format: "%.1f", star))
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(Color(red: 0.8, green: 0.6, blue: 0.7, opacity: 0.7))
+    //                                        .padding(.top, 1)
+                                    }
+    //                                .padding(.bottom, 40)
                                     
-                                    Button {
-                                        print("push\(documents[index].reference)")
-                                        documents[index].reference.delete { error in
-                                            if let error = error {
-                                                print("Error deleting document: \(error)")
-                                            } else {
-                                                print("Document successfully deleted.")
-                                                fetchDocuments()
+                                    HStack {
+                                        Text("$\(price)")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(Color(red: 0.8, green: 0.5, blue: 0.1))
+                                        
+                                        Spacer()
+                                        
+                                        Button {
+                                            print("push\(documents[index].reference)")
+                                            documents[index].reference.delete { error in
+                                                if let error = error {
+                                                    print("Error deleting document: \(error)")
+                                                } else {
+                                                    print("Document successfully deleted.")
+                                                    fetchDocuments()
+                                                }
                                             }
+                                        } label: {
+                                            Image(systemName: "minus")
+                                                .foregroundColor(.white)
+                                                .padding(.all, 8)
+                                                .padding(.top, 6)
+                                                .padding(.bottom, 6)
+                                                .background(Color(red: 0.8, green: 0.6, blue: 0.7, opacity: 0.9))
+                                                .cornerRadius(100)
                                         }
-                                    } label: {
-                                        Image(systemName: "minus")
-                                            .foregroundColor(.white)
-                                            .padding(.all, 8)
-                                            .padding(.top, 7)
-                                            .padding(.bottom, 7)
-                                            .background(Color(red: 0.8, green: 0.6, blue: 0.7, opacity: 0.9))
-                                            .cornerRadius(100)
                                     }
                                 }
+                                .padding(.all, 20)
                             }
-                            .padding(.all, 20)
+                            .padding(.bottom, 10)
                         }
-                        .padding(.bottom, 10)
                     }
                 }
             }
+            .frame(maxWidth: .infinity)
             .padding()
             .padding(.leading, 5)
             .background(.white)
@@ -109,7 +118,7 @@ struct CartView: View {
     
     private func fetchDocuments() {
         let db = Firestore.firestore()
-        db.collection("cart").getDocuments { snapshot, error in
+        db.collection("users").document(userID).collection("cart").getDocuments { snapshot, error in
             if let error = error {
                 print("Error fetching documents: \(error)")
                 return
@@ -122,6 +131,19 @@ struct CartView: View {
             
             self.documents = documents
         }
+//        db.collection("cart").getDocuments { snapshot, error in
+//            if let error = error {
+//                print("Error fetching documents: \(error)")
+//                return
+//            }
+//
+//            guard let documents = snapshot?.documents else {
+//                print("No documents found")
+//                return
+//            }
+//
+//            self.documents = documents
+//        }
     }
 }
 
